8f97221f6418e7726dec383502dd0c50f8db1354
Cryptomental <cryptomental.com@gmail.com>
2018-08-30 22:23:58 +0200

    cleanup9

--- appveyor/variables.ps1
+++ appveyor/variables.ps1
@@ -1,36 +1,6 @@
-# Input environment variables:
-#   arch: Architecture (32 or 64)
-#   msvc: Microsoft Visual C++ version (11, 12, 14)
-#   appveyor_repo_tag_name: AppVeyor tag (Vim tags are in vX.Y.Z form)
-#   lua_version: Lua version
-#   perl_version: Perl version
-#   python2_version: Python 2 version
-#   python3_version: Python 3 version
-#   racket_version: Racket version
-#   ruby_version: Ruby version
-#   tcl_version: Tcl version
-#   bintray_username: Bintray username
-#
-# Output environment variables:
-#   vim_version: Vim version in X.Y.Z form
-#   vim_artifact: Vim executable name
-#   vim_tweet: message send on Twitter
-#   git_description: release notes on GitHub
-#   bintray_description: description on Bintray
-
 Function GetMajorMinorVersion($version) {
     $version_array = $version.Split('.')
     $version_array[0] + '.' + $version_array[1]
-}
-
-If ($env:arch -eq 32) {
-    $vim_arch = "x86"
-    $perl_version = $env:perl32_version
-    $tcl_version = $env:tcl32_version
-} Else {
-    $vim_arch = "x64"
-    $perl_version = $env:perl64_version
-    $tcl_version = $env:tcl64_version
 }
 
 $msvc = $env:appveyor_build_worker_image.Substring($env:appveyor_build_worker_image.Length-4)
